3eabb533222165314baf1d2390d1a076da66baa1
Dregorio <Dregorio@DESKTOP>
2017-07-11 20:09:37 +0200

    init

---
+++ SQLConnection/ConnectionPostgre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLConnection
+{
+    class ConnectionPostgre : Connection
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return String.Format("host=" + Host + ";username=" + Username + ";password=" + Password + ";database=" + Database);
+                //Host=localhost;Username=postgres;Password=haslo;Database=Testy
+            }
+        }
+    }
+}
---
+++ SQLConnection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+namespace SQLConnection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            var value = configuration.GetValue<string>("appsettings:host", "local");
+            Console.WriteLine(value);
+
+
+            var connection = new ConnectionPostgre();
+            configuration.GetSection("appSettings").Bind(connection);
+            Console.WriteLine($"{connection.ConnectionString}");
+            var c = connection.ConnectionString;
+
+
+            var connectionString = configuration["connectionStrings:MyDb"];
+            Console.WriteLine(connectionString);
+
+            using (var conn = new NpgsqlConnection(c))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "select * from contractors";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[2]} {reader[3]}");
+                        }
+                    }
+                }
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
